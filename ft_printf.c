688b0b4f14ca6ef236e7a9c8779532abfd7b2764
Yaren Sakarya <ysakarya@k1m32s09.42istanbul.com.tr>
2025-01-05 20:09:37 +0300

    deneme

--- ft_printf.c
+++ ft_printf.c
@@ -1,6 +1,6 @@
 #include "ft_printf.h"
 
-static int	ft_putbase(char c, long long x, int *count, char *s)
+static void	ft_putbase(char c, long long x, int *count, char *s)
 {
 	int	a;
 
@@ -13,44 +13,44 @@ static int	ft_putbase(char c, long long x, int *count, char *s)
 			x = -x;
 		}
 	}
-	else if (c == 'p')
-	{
-		*count += write(1, "0x", 2);
-		a = 16;
-	}
+	else if (c == 'u')
+		a = 10;
 	else
 		a = 16;
 
 	if (x / a)
 		ft_putbase(c, x / a, count, s);
 	*count += write(1, &s[x % a], 1);
-	return *count;
+}
+
+static void ft_putstr(char *str, int *count)
+{
+	while (*str)
+			*count += write(1, str++, 1);
 }
 
 static void	ft_filter(char c, int *count, va_list args)
 {
-	char	*str;
-
 	if (c == 'c')
 	{
 		c = va_arg(args, int);
 		*count += write(1, &c, 1);
 	}
 	else if (c == 's')
-	{
-		str = va_arg(args, char *);
-		while (*str)
-			*count += write(1, str++, 1);
-	}
+		ft_putstr(va_arg(args, char*), count);
 	else if (c == 'p')
+	{
+		*count += write(1, "0x", 2);
 		ft_putbase(c, va_arg(args, unsigned long), count, "0123456789abcdef");
+	}	
 	else if (c == 'd' || c == 'i')
 		ft_putbase(c, va_arg(args, int), count, "0123456789");
 	else if (c == 'u')
 		ft_putbase(c, va_arg(args, unsigned int), count, "0123456789");
-	else if (c == 'x' || c == 'X')
-		ft_putbase(c, va_arg(args, unsigned int), count, 
-			(c == 'x' ? "0123456789abcdef" : "0123456789ABCDEF"));
+	else if (c == 'x')
+		ft_putbase(c, va_arg(args, unsigned int), count, "0123456789abcdef");
+	else if (c == 'X')
+		ft_putbase(c, va_arg(args, unsigned int), count, "0123456789ABCDEF");
 	else if (c == '%')
 		*count += write(1, &c, 1);
 }
